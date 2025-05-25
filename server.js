757da82d13c201f195f439604157e3adc11b57c3
hannaso91 <hannaso@hiof.no>
2025-05-25 17:21:11 +0200

    pusher

--- server.js
+++ server.js
@@ -30,63 +30,6 @@ async function makeRequest(url) {
   }
 }
 
-// interface SearchFeature {
-//   properties : {
-//     tittel: string;
-//     lenke: string;
-//     beskrivelse: string;
-//   }
-// }
-
-
-// interface SearchResponse {
-//   features: SearchFeature[];
-// }
-
-// const searchBrave = defineFunction({
-//   name: 'searchBrave',
-//   description: 'Søk etter informasjon via Brave search API',
-//   inputSchema: z.object({
-//     q: z.string().describe('Søkeordet brukeren vil søke på')
-//   }),
-//   outputSchema: z.object({
-//     resultater: z.array(
-//       z.object({
-//         tittel: z.string(),
-//         lenke: z.string(),
-//         beskrivelse: z.string()
-//       })
-//     )
-//   }),
-//   handler: async ({q}) => {
-//     const params = new URLSearchParams({q})
-
-//     const response = await fetch(`https://api.search.brave.com/res/v1/web/search?${params}`, {
-//       headers: {
-//         'Accept': 'application/json',
-//         'X-Subscription-Token': 'BSAywgQshX3Hr_uSmrMbevOf7RTkVrj'
-//       }
-//     });
-
-//     const data = await response.json()
-
-//     const resultater = data.web?.results?.map(r => ({
-//       tittel: r.title,
-//       lenke: r.url,
-//       beskrivelse: r.description
-//     })) || []
-
-//     return {resultater}
-//   }
-// })
-
-// createServer({
-//   functions: [searchBrave]
-// }).listen(3000, () => {
-//   console.log(`MCP-server kjører på localhost:3000`)
-// })
-
-// Register weather tools
 server.tool(
   "search",
   "Hent søkeresultat",
